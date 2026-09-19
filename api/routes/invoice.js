01fc78a5a545117215385c99ca2f041bb1cd4393
Thiri Win <thiriwinzhuang@gmail.com>
2026-09-19 12:16:52 +0630

    invoice single data route is added

--- api/routes/invoice.js
+++ api/routes/invoice.js
@@ -82,6 +82,36 @@ router.get('/export/excel', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+
+        if (isNaN(id)) {
+            return res.status(400).json({ error: 'Invalid invoice' })
+        }
+
+        const invoice = await prisma.invoice.findUnique({
+            where: { id },
+            include: {
+                customer: true,
+                car: true,
+                details: {
+                    include: {
+                        item: true
+                    }
+                }
+            }
+        })
+        if (!invoice) {
+            return res.status(404).json({ error: 'Invoice not found' });
+        }
+        res.status(200).json({ message: "Invoice is ready", data: invoice });
+    } catch (error) {
+        console.error('error', error);
+        res.status(500).json({ error: error.message });
+    }
+})
+
 async function processCustomer(tx, customerData) {
     if (!customerData || !customerData.name) return null;
     let customer = await tx.customer.findFirst({
@@ -338,6 +368,7 @@ router.get('/', async (req, res) => {
         const totalPages = Math.ceil(totalItems / limitNum);
 
         res.status(200).json({
+            message: 'Invoices',
             data: invoices,
             pagination: {
                 total_items: totalItems,
