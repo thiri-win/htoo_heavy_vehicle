72f78857ec056e97ffbe1734ba062ee5393df409
Thiri Win <thiriwinzhuang@gmail.com>
2026-09-20 10:23:44 +0630

    add export app in index.js

--- api/index.js
+++ api/index.js
@@ -21,6 +21,8 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: "API is running successfully!" });
 });
 
+module.exports = app;
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running localhost:${PORT}`);
